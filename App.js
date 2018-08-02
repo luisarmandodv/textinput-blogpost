a1076ab6fe54b4566cc1825aa5229ade59b628c7
luisarmandodv <luisarmandodv@gmail.com>
2018-08-02 17:34:16 -0500

    feature/select-next-textinput-using-refs

--- App.js
+++ App.js
@@ -1,49 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.textInput = {};
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  focusNextTextInput(id) {
+    this.textInput[id].focus();
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.outerContainer}>
+        <TextInput
+          placeholder="one"
+          onSubmitEditing={() => {
+            this.focusNextTextInput("two");
+          }}
+          style={styles.textInput}
+          ref={input => {
+            this.textInput["one"] = input;
+          }}
+        />
+        <TextInput
+          placeholder="two"
+          onSubmitEditing={() => {
+            this.focusNextTextInput("three");
+          }}
+          style={styles.textInput}
+          ref={input => {
+            this.textInput["two"] = input;
+          }}
+        />
+        <TextInput
+          placeholder="three"
+          onSubmitEditing={() => {
+            this.focusNextTextInput("four");
+          }}
+          style={styles.textInput}
+          ref={input => {
+            this.textInput["three"] = input;
+          }}
+        />
+        <TextInput
+          placeholder="four"
+          style={styles.textInput}
+          ref={input => {
+            this.textInput["four"] = input;
+          }}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outerContainer: {
+    alignItems: "center",
+    backgroundColor: "#ecf0f1",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: "column",
+    paddingTop: 40
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  textInput: {
+    alignSelf: "stretch",
+    backgroundColor: "#ffffff",
+    height: 50,
+    marginBottom: 20,
+    marginHorizontal: 10,
+    paddingHorizontal: 10
+  }
 });
